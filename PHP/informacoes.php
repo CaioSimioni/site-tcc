00dd1e36f3d67710efb8a4eb70f7d23b32ef9135
CaioSimioni <83130766+CaioSimioni@users.noreply.github.com>
2021-06-04 15:56:14 -0300

    Add files via upload

---
+++ PHP/informacoes.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>POLO</title>
+    <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="../CSS/styleinformacoes.css">
+    <link rel="shortcut icon" href="../ASSETS/LogoArara.png" type="image/x-icon">
+</head>
+<body>
+    <div id="global">
+
+        <?php
+            include "../HTML/cabecalho.html";
+        ?>
+
+        <div class="div_principal">
+            <div class="card">
+                <h1>O que é o Polo?</h1>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate omnis praesentium, sint rem odio, laboriosam dolore modi quos ea, ipsam impedit alias perferendis magnam corporis. Ratione quibusdam mollitia eius quas?</p>
+            </div>
+            <div class="card">
+                <h1>Por que usar o Polo?</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt saepe ipsam perferendis sunt accusamus inventore iste. Deserunt, perferendis. Dolorum, deserunt commodi facere ducimus laboriosam id consequuntur deleniti doloremque ea qui.</p>
+            </div>
+            <div class="card">
+                <h1>Não sei oque colocar aqui</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, quam, cupiditate voluptates quis quaerat omnis, molestiae accusamus provident alias dolorum sint iusto rerum repudiandae voluptatibus modi cumque quod soluta natus.</p>
+            </div>
+        </div>
+
+        <hr>
+
+        <div class="div_mid">
+            
+        </div>
+        <?php
+            include "../HTML/rodape.html";
+        ?>
+
+    </div>
+</body>
+</html>
