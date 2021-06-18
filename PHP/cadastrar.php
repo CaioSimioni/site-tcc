db5cd434e75e85710fb7e9c1009dbe13296974cd
zMango <caioribeirosimioni@hotmail.com>
2021-06-18 20:45:14 -0300

    n sei '-'

--- PHP/cadastrar.php
+++ PHP/cadastrar.php
@@ -11,7 +11,7 @@ $u = new Usuario;   // Crio um objeto de Usuario
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>POLO</title>
-    <link rel="stylesheet" href="../css/style_cadastro.css">
+    <link rel="stylesheet" href="../css/style_cadastrar.css">
     <link rel="shortcut icon" href="../ASSETS/LogoArara.png" type="image/x-icon">
 </head>
 <body>
--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>POLO</title>
-    <link rel="stylesheet" href="./CSS/style_index.css">
+    <link rel="stylesheet" href="./CSS/style_indexx.css">
     <link rel="shortcut icon" href="./ASSETS/LogoArara.png" type="image/x-icon">
 </head>
 
