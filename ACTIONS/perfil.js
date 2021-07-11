7debfcab43c3ff623e5f1ccd892eb00a1404914b
zMango <caioribeirosimioni@hotmail.com>
2021-07-11 15:15:44 -0300

    escrevendo o nome do arquivo no form do perfil

--- ACTIONS/perfil.js
+++ ACTIONS/perfil.js
@@ -25,8 +25,17 @@ label.addEventListener('click', () => {
     
 });
 
-let name_arquivo = document.getElementById('nome-arquivo');
-
 function mostrarNomeImagem(){
-    name_arquivo.innerHTML = "<p><?php $novo_nome?></p>"
+    let name_arquivo_div = document.getElementById('nome-arquivo');
+    let name_arquivo = document.getElementById('file').value;
+    name_arquivo_div.innerHTML = `<p class="name-arquivo">${extrairArquivo(name_arquivo)}</p>`;
+}
+
+function extrairArquivo(fullPath){
+    var startIndex = (fullPath.indexOf('\\') >= 0 ? fullPath.lastIndexOf('\\') : fullPath.lastIndexOf('/'));
+    var filename = fullPath.substring(startIndex);
+    if (filename.indexOf('\\') === 0 || filename.indexOf('/') === 0) {
+        filename = filename.substring(1);
+    }
+    return filename;
 }
