ab3d9dafd790f91a0ac30fd2b48a1cd9e1cbb4e0
zMango <caioribeirosimioni@hotmail.com>
2021-07-15 16:05:13 -0300

    Update usuario.php

--- User/usuario.php
+++ User/usuario.php
@@ -89,15 +89,6 @@ Class Usuario{
         }
     }
 
-    public function sairUsuario(){
-        unset($_SESSION['codigo_usuario']);
-        unset($_SESSION['nome_usuario']);
-        unset($_SESSION['email_usuario']);
-        unset($_SESSION['imagem']);
-        unset($_SESSION['logged']);
-        session_destroy();
-        echo "<script> window.location.href = '../index.php'</script>";
-    }
 }
 
 ?>
