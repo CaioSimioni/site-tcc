dd1ac5c334f1988327ddf4219932c9adc6077633
zMango <caioribeirosimioni@hotmail.com>
2021-07-15 16:35:16 -0300

    Update noticiaCadastro.php

--- Admin/noticiaCadastro.php
+++ Admin/noticiaCadastro.php
@@ -14,18 +14,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Polo</title>
+    <link rel="stylesheet" href="../Css/style_noticiaADM.css">
+    <link rel="shortcut icon" href="../Materials/polo_icon.png" type="image/x-icon">
 </head>
 <body>
     <?php
         include "../Templates/cabecalho.php";
     ?>
     <div id="global">
-        <form method="POST" action="noticiaCadastro.php">
-            Título<input type="text">
-            Descrição<input type="text">
-            Data<input type="date">
-            imagem<input type="file">
-            <input type="submit">
+        <form method="POST" action="noticiaCadastro.php" id="forms_newNoticia">
+            Título<input type="text" class="atrib_noticia">
+            Descrição<input type="text" class="atrib_noticia">
+            Data<input type="date" class="atrib_noticia">
+            imagem<input type="file" class="atrib_noticia">
+            <input type="submit" class="atrib_noticia">
         </form>
     </div>
     <?php
