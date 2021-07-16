6fc5faa3ad805f2abf15636175418f4c08cd985a
zMango <caioribeirosimioni@hotmail.com>
2021-07-16 16:10:08 -0300

    Update noticia.php

--- Noticia/noticia.php
+++ Noticia/noticia.php
@@ -20,12 +20,12 @@ class Noticia{
         
         global $pdo;
         $sql = $pdo->prepare("INSERT INTO noticia(fonte,data,descricao,titulo,imagem) value('$fonte',now(),'$descricao','$titulo',''");
-        $sql->execute()
+        $sql->execute();
     }
 
     public function editarNoticia ($id_noticia, $titulo, $descricao){
         global $pdo;
-
+    }
 
 
     public function excluirNoticia ($id_noticia){
