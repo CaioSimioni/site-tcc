7d159c7f7d06f8fd77eede134edf040e5e30d2e7
zMango <83130766+CaioSimioni@users.noreply.github.com>
2021-07-27 18:22:16 -0300

    Update home.js

--- Js/home.js
+++ Js/home.js
@@ -1,15 +1,7 @@
-let card = document.querySelectorAll(".card")
+let card = document.getElementsByClassName("card")
 let box = document.getElementById("popup");
 
 
-var myFunction = function() {
-    var attribute = this.getAttribute("data-myattribute");
-    alert(attribute);
-};
-
-card.forEach(el => el.addEventListener('click', event => {
-    console.log(event.target.getAttribute("data-el"));
-}));
 
 box.addEventListener("click", () =>{
     box.classList.remove("visivel");
