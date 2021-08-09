fc12092c3d86cd181b6d81cfcd3b53fafd73f7d3
zMango <83130766+CaioSimioni@users.noreply.github.com>
2021-08-09 18:42:01 -0300

    Update adm.js

--- Js/adm.js
+++ Js/adm.js
@@ -1,7 +1,17 @@
-function newNot (){
+
+
+function newNot(){
     window.location.href = "../Noticia/noticiaCadastro.php";
 }
 
 function selectNot(){
     window.location.href = "../Noticia/noticiaSelecionar.php";
+}
+
+function newUser(){
+    window.location.href = "../Usuario/usuarioCadastrar.php";
+}
+
+function selectUser(){
+    window.location.href = "../Usuario/usuarioSelecionar.php";
 }
