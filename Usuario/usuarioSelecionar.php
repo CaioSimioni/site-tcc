4662e509624dfbc014ce679bdc0b685f36772647
zMango <83130766+CaioSimioni@users.noreply.github.com>
2021-08-09 18:42:29 -0300

    Create usuarioSelecionar.php

---
+++ Usuario/usuarioSelecionar.php
@@ -0,0 +1,67 @@
+<?php
+    require "../System/classes.php";
+    $banco = new BancoBD;
+    $usuario = new Usuario;
+
+    $banco->conectar();
+
+    if(!isset($_SESSION['logged']) or !isset($_SESSION['codigo_usuario'])){
+        echo "<script> alert('Falha na autenticação de usuário!')</script>";    
+        echo "<script>window.location.href='../index.php'</script>";
+        exit;
+    }
+
+    if($_SESSION['adm'] == false) {
+        echo "<script>alert('Você não tem permissão para entrar nesta área')</script>";
+        echo "<script>window.location.href='../index.php'</script>";
+        exit;
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Polo</title>
+    <link rel="shortcut icon" href="../Materials/polo_icon.png" type="image/x-icon">
+    <link rel="stylesheet" href="../Css/style_selecionarUsuario.css">
+</head>
+<body>
+    
+    <?php include "../Templates/cabecalho.php"; ?>
+
+    <div id="global">
+        <section>
+            <table>
+                <thead>
+                    <tr>
+                        <td>Código</td>
+                        <td>Nome</td>
+                        <td>Cargo</td>
+                        <td>Funções</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        if($banco->conectar()){
+
+                            if($usuario->selecionarUsuarios()){
+
+                            }else{
+
+                            }
+
+                        }
+                    ?>
+                </tbody>
+            </table>
+        </section>
+    </div>
+
+    <?php include "../Templates/rodape.php"; ?>
+
+</body>
+</html>
