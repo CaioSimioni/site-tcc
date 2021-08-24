0427f187edb4f2880e42d0a548414f307f1c916b
zMango <83130766+CaioSimioni@users.noreply.github.com>
2021-08-24 16:21:23 -0300

    Update usuarioCadastrar.php

--- Usuario/usuarioCadastrar.php
+++ Usuario/usuarioCadastrar.php
@@ -26,7 +26,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Polo</title>
     <link rel="shortcut icon" href="../Materials/polo_icon.png" type="image/x-icon">
-    <link rel="stylesheet" href="../Css/style_cadastrarUsuario.css">
+    <link rel="stylesheet" href="../Css/style_usuarioCadastrar.css">
 </head>
 <body>
 
