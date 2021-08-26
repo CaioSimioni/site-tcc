52336d41daeb36a172bfd01f90679a8d9d22ef98
zMango <83130766+CaioSimioni@users.noreply.github.com>
2021-08-26 18:27:09 -0300

    Update deletaNoticia.php

--- Noticia/deletaNoticia.php
+++ Noticia/deletaNoticia.php
@@ -22,5 +22,6 @@ $cod = $_GET['idnoticia'];
 $sql=$pdo->prepare( "DELETE FROM `noticia` WHERE `noticia`.`id_noticia` = '$cod'");
 $sql->execute();
 echo "<script>window.location.href='noticiaSelecionar.php'</script>";
+exit;
 ?>
 
