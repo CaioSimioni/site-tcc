0ae88cf87158f88f0c807097f99887680c3d3338
zMango <83130766+CaioSimioni@users.noreply.github.com>
2021-08-26 19:47:35 -0300

    Update admin.php

--- Admin/admin.php
+++ Admin/admin.php
@@ -38,47 +38,58 @@ if($_SESSION['adm'] == false) {
         <section>
             <h1>Moderação</h1>
             <div class="cards">
+
                 <div class="box">
                     <div class="titulo-box-card">
                         <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF"><g><path d="M0,0h24v24H0V0z" fill="none"/></g><g><path d="M16,3H5C3.9,3,3,3.9,3,5v14c0,1.1,0.9,2,2,2h14c1.1,0,2-0.9,2-2V8L16,3z M7,7h5v2H7V7z M17,17H7v-2h10V17z M17,13H7v-2h10 V13z M15,9V5l4,4H15z"/></g></svg>
                         <p class="title">Noticias</p>
                     </div>
-                    
                     <div class="objs">
-
                         <div onclick="newNot()" class="item novo" id="new-not">
                             <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF"><g><rect fill="none" height="24" width="24"/></g><g><g><path d="M19,13h-6v6h-2v-6H5v-2h6V5h2v6h6V13z"/></g></g></svg>
                             <p>Nova Notícia</p>
                         </div>
-
                         <div onclick="selectNot()" class="item sele" id="sele-not">
                             <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/></svg>
                             <p>Selecionar Notícias</p>
                         </div>
-
                     </div>
                 </div>
+
                 <div class="box">
                     <div class="titulo-box-card">
                     <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"/></svg>
                         <p class="title">Usuários</p>
                     </div>
                     <div class="objs">
-
                         <div onclick="newUser()" class="item novo" id="new-user">
                             <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF"><g><rect fill="none" height="24" width="24"/></g><g><g><path d="M19,13h-6v6h-2v-6H5v-2h6V5h2v6h6V13z"/></g></g></svg>
                             <p>Novo Usuário</p>
                         </div>
-
                         <div onclick="selectUser()" class="item sele" id="sele-user">
                             <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/></svg>
                             <p>Selecionar Usuários</p>
                         </div>
-
                     </div>
                 </div>
-                
+
+                <div class="box">
+                    <div class="titulo-box-card">
+                    <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF"><rect fill="none" height="24" width="24"/><path d="M19,5h-2V3H7v2H5C3.9,5,3,5.9,3,7v1c0,2.55,1.92,4.63,4.39,4.94c0.63,1.5,1.98,2.63,3.61,2.96V19H7v2h10v-2h-4v-3.1 c1.63-0.33,2.98-1.46,3.61-2.96C19.08,12.63,21,10.55,21,8V7C21,5.9,20.1,5,19,5z M5,8V7h2v3.82C5.84,10.4,5,9.3,5,8z M19,8 c0,1.3-0.84,2.4-2,2.82V7h2V8z"/></svg>
+                        <p class="title">E-sports</p>
+                    </div>
+                    <div class="objs">
+                        <div onclick="newCamp()" class="item novo" id="new-camp">
+                            <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF"><g><rect fill="none" height="24" width="24"/></g><g><g><path d="M19,13h-6v6h-2v-6H5v-2h6V5h2v6h6V13z"/></g></g></svg>
+                            <p>Novo Campeonato</p>
+                        </div>
+                        <div onclick="selectCamp()" class="item sele" id="sele-camp">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/></svg>
+                            <p>Selecionar Campeonato</p>
+                        </div>
+                    </div>
                 </div>
+                <!-- Adicionar mais possibilidades aqui -->
             </div>
         </section>
     </div>
@@ -102,6 +113,14 @@ if($_SESSION['adm'] == false) {
 
         function selectUser(){
             window.location.href = "../Usuario/usuarioSelecionar.php";
+        }
+
+        function newCamp(){
+            window.location.href = "../Esports/campeonatoCadastrar.php";
+        }
+
+        function selectCamp(){
+            window.location.href = "../Esports/campeonatoSelecionar.php";
         }
     </script>
 </body>
