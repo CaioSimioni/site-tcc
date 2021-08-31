fbfb9a2394e817325e757324199f2abcdfb09ac1
zMango <83130766+CaioSimioni@users.noreply.github.com>
2021-08-31 16:20:29 -0300

    Update setTab.php

--- Test/setTab.php
+++ Test/setTab.php
@@ -6,7 +6,7 @@ if (!isset($_POST['tabCamp'])) {
 }
 
 var_dump($_POST);
-file_put_contents($_POST['nameTabCamp'], $_POST['tabCamp']);
+file_put_contents('tabs_camps\\' . $_POST['nameTabCamp'] . '.php', $_POST['tabCamp']);
 
 echo "<button><a href='index.php'>Voltar</a></button>";
 
