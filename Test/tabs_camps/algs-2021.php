856ab472592c38fd7a79fa2aa5072801d00e6c20
zMango <83130766+CaioSimioni@users.noreply.github.com>
2021-09-03 17:16:20 -0300

    Update algs-2021.php

--- Test/tabs_camps/algs-2021.php
+++ Test/tabs_camps/algs-2021.php
@@ -1,16 +1,6 @@
 <table>
- 	<thead>
-		<th>Código</th>
-		<th>Nome</th>
-	</thead>
- 	<tbody>
-  		<tr>
-   			<td>1</td>
-   			<td>teste 1</td>
-  		</tr>
-  		<tr>
-   			<td>2</td>
-   			<td>teste 2</td>
-  		</tr>
- 	</tbody>
+<thead>
+</thead>
+<tbody>
+</tbody>
 </table>
