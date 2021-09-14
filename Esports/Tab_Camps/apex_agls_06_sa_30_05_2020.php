16c3971e4f2519c2c4d4117bb58e7b425c35b7ad
zMango <83130766+CaioSimioni@users.noreply.github.com>
2021-09-14 01:08:09 -0300

    Create apex_agls_06_sa_30_05_2020.php

---
+++ Esports/Tab_Camps/apex_agls_06_sa_30_05_2020.php
@@ -0,0 +1,436 @@
+<table>
+	<thead>
+		<tr class="title">
+			<th colspan="20">Finals ALGS Online #6 - South America, Prize pool: $9,000 USD</th>
+		</tr>
+		<tr>
+			<th rowspan="2"></th>
+			<th rowspan="2">Time</th>
+			<th rowspan="2">Total</th>
+			<th colspan="2">Round 1</th>
+			<th colspan="2">Round 2</th>
+			<th colspan="2">Round 3</th>
+			<th colspan="2">Round 4</th>
+			<th colspan="2">Round 5</th>
+		</tr>
+		<tr>
+			<!-- round 1 -->
+			<th>P</th>
+			<th>K</th>
+			<!-- round 2 -->
+			<th>P</th>
+			<th>K</th>
+			<!-- round 3 -->
+			<th>P</th>
+			<th>K</th>
+			<!-- round 4 -->
+			<th>P</th>
+			<th>K</th>
+			<!-- round 5 -->
+			<th>P</th>
+			<th>K</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>1.</td>
+			<td>Sloty Sloty</td>
+			<td>52</td>
+			<!-- round 1 -->
+			<td>8</td>
+			<td>8</td>
+			<!-- round 2 -->
+			<td>4</td>
+			<td>12</td>
+			<!-- round 3 -->
+			<td>2</td>
+			<td>5</td>
+			<!-- round 4 -->
+			<td>7</td>
+			<td>4</td>
+			<!-- round 5 -->
+			<td>6</td>
+			<td>1</td>
+		</tr>
+		<tr>
+			<td>2.</td>
+			<td>Otakus Fedidos</td>
+			<td>52</td>
+			<!-- round 1 -->
+			<td>11</td>
+			<td>3</td>
+			<!-- round 2 -->
+			<td>10</td>
+			<td>6</td>
+			<!-- round 3 -->
+			<td>1</td>
+			<td>15</td>
+			<!-- round 4 -->
+			<td>7</td>
+			<td>4</td>
+			<!-- round 5 -->
+			<td>6</td>
+			<td>1</td>
+		</tr>
+		<tr>
+			<td>3.</td>
+			<td>Millions</td>
+			<td>52</td>
+			<!-- round 1 -->
+			<td>1</td>
+			<td>8</td>
+			<!-- round 2 -->
+			<td>11</td>
+			<td>3</td>
+			<!-- round 3 -->
+			<td>3</td>
+			<td>4</td>
+			<!-- round 4 -->
+			<td>5</td>
+			<td>3</td>
+			<!-- round 5 -->
+			<td>2</td>
+			<td>1</td>
+		</tr>
+		<tr>
+			<td>4.</td>
+			<td>Fearow</td>
+			<td>50</td>
+			<!-- round 1 -->
+			<td>5</td>
+			<td>6</td>
+			<!-- round 2 -->
+			<td>2</td>
+			<td>4</td>
+			<!-- round 3 -->
+			<td>9</td>
+			<td>6</td>
+			<!-- round 4 -->
+			<td>18</td>
+			<td>1</td>
+			<!-- round 5 -->
+			<td>3</td>
+			<td>11</td>
+		</tr>
+		<tr>
+			<td>5.</td>
+			<td>LRG</td>
+			<td>49</td>
+			<!-- round 1 -->
+			<td>14</td>
+			<td>0</td>
+			<!-- round 2 -->
+			<td>1</td>
+			<td>7</td>
+			<!-- round 3 -->
+			<td>7</td>
+			<td>3</td>
+			<!-- round 4 -->
+			<td>1</td>
+			<td>10</td>
+			<!-- round 5 -->
+			<td>14</td>
+			<td>0</td>
+		</tr>
+		<tr>
+			<td>6.</td>
+			<td>PATULHA</td>
+			<td>42</td>
+			<!-- round 1 -->
+			<td>2</td>
+			<td>5</td>
+			<!-- round 2 -->
+			<td>3</td>
+			<td>6</td>
+			<!-- round 3 -->
+			<td>6</td>
+			<td>3</td>
+			<!-- round 4 -->
+			<td>4</td>
+			<td>3</td>
+			<!-- round 5 -->
+			<td>12</td>
+			<td>0</td>
+		</tr>
+		<tr>
+			<td>7.</td>
+			<td>LTG</td>
+			<td>39</td>
+			<!-- round 1 -->
+			<td>16</td>
+			<td>3</td>
+			<!-- round 2 -->
+			<td>6</td>
+			<td>0</td>
+			<!-- round 3 -->
+			<td>13</td>
+			<td>0</td>
+			<!-- round 4 -->
+			<td>16</td>
+			<td>3</td>
+			<!-- round 5 -->
+			<td>1</td>
+			<td>17</td>
+		</tr>
+		<tr>
+			<td>8.</td>
+			<td>TENGAA</td>
+			<td>35</td>
+			<!-- round 1 -->
+			<td>6</td>
+			<td>5</td>
+			<!-- round 2 -->
+			<td>15</td>
+			<td>1</td>
+			<!-- round 3 -->
+			<td>5</td>
+			<td>7</td>
+			<!-- round 4 -->
+			<td>8</td>
+			<td>1</td>
+			<!-- round 5 -->
+			<td>4</td>
+			<td>6</td>
+		</tr>
+		<tr>
+			<td>9.</td>
+			<td>No Pain No Gain</td>
+			<td>32</td>
+			<!-- round 1 -->
+			<td>9</td>
+			<td>4</td>
+			<!-- round 2 -->
+			<td>14</td>
+			<td>0</td>
+			<!-- round 3 -->
+			<td>12</td>
+			<td>5</td>
+			<!-- round 4 -->
+			<td>3</td>
+			<td>7</td>
+			<!-- round 5 -->
+			<td>10</td>
+			<td>3</td>
+		</tr>
+		<tr>
+			<td>10.</td>
+			<td>MoTion Team</td>
+			<td>24</td>
+			<!-- round 1 -->
+			<td>3</td>
+			<td>2</td>
+			<!-- round 2 -->
+			<td>12</td>
+			<td>0</td>
+			<!-- round 3 -->
+			<td>20</td>
+			<td>0</td>
+			<!-- round 4 -->
+			<td>6</td>
+			<td>7</td>
+			<!-- round 5 -->
+			<td>9</td>
+			<td>2</td>
+		</tr>
+		<tr>
+			<td>11.</td>
+			<td>Reapers</td>
+			<td>23</td>
+			<!-- round 1 -->
+			<td>12</td>
+			<td>2</td>
+			<!-- round 2 -->
+			<td>9</td>
+			<td>1</td>
+			<!-- round 3 -->
+			<td>4</td>
+			<td>1</td>
+			<!-- round 4 -->
+			<td>11</td>
+			<td>7</td>
+			<!-- round 5 -->
+			<td>15</td>
+			<td>2</td>
+		</tr>
+		<tr>
+			<td>12.</td>
+			<td>GHOSTS E-Sports</td>
+			<td>21</td>
+			<!-- round 1 -->
+			<td>4</td>
+			<td>2</td>
+			<!-- round 2 -->
+			<td>5</td>
+			<td>1</td>
+			<!-- round 3 -->
+			<td>15</td>
+			<td>1</td>
+			<!-- round 4 -->
+			<td>20</td>
+			<td>0</td>
+			<!-- round 5 -->
+			<td>13</td>
+			<td>6</td>
+		</tr>
+		<tr>
+			<td>13.</td>
+			<td>SFC</td>
+			<td>18</td>
+			<!-- round 1 -->
+			<td>10</td>
+			<td>5</td>
+			<!-- round 2 -->
+			<td>19</td>
+			<td>4</td>
+			<!-- round 3 -->
+			<td>18</td>
+			<td>3</td>
+			<!-- round 4 -->
+			<td>19</td>
+			<td>3</td>
+			<!-- round 5 -->
+			<td>19</td>
+			<td>1</td>
+		</tr>
+		<tr>
+			<td>14.</td>
+			<td>Golden Dogs</td>
+			<td>18</td>
+			<!-- round 1 -->
+			<td>7</td>
+			<td>1</td>
+			<!-- round 2 -->
+			<td>7</td>
+			<td>3</td>
+			<!-- round 3 -->
+			<td>10</td>
+			<td>1</td>
+			<!-- round 4 -->
+			<td>10</td>
+			<td>0</td>
+			<!-- round 5 -->
+			<td>8</td>
+			<td>1</td>
+		</tr>
+		<tr>
+			<td>15.</td>
+			<td>Aim xtrem3</td>
+			<td>12</td>
+			<!-- round 1 -->
+			<td>20</td>
+			<td>0</td>
+			<!-- round 2 -->
+			<td>17</td>
+			<td>1</td>
+			<!-- round 3 -->
+			<td>8</td>
+			<td>0</td>
+			<!-- round 4 -->
+			<td>14</td>
+			<td>0</td>
+			<!-- round 5 -->
+			<td>7</td>
+			<td>5</td>
+		</tr>
+		<tr>
+			<td>16.</td>
+			<td>No Rage Quit</td>
+			<td>11</td>
+			<!-- round 1 -->
+			<td>13</td>
+			<td>0</td>
+			<!-- round 2 -->
+			<td>13</td>
+			<td>3</td>
+			<!-- round 3 -->
+			<td>11</td>
+			<td>1</td>
+			<!-- round 4 -->
+			<td>13</td>
+			<td>3</td>
+			<!-- round 5 -->
+			<td>16</td>
+			<td>0</td>
+		</tr>
+		<tr>
+			<td>17.</td>
+			<td>MPSB</td>
+			<td>8</td>
+			<!-- round 1 -->
+			<td>19</td>
+			<td>1</td>
+			<!-- round 2 -->
+			<td>16</td>
+			<td>1</td>
+			<!-- round 3 -->
+			<td>14</td>
+			<td>1</td>
+			<!-- round 4 -->
+			<td>9</td>
+			<td>2</td>
+			<!-- round 5 -->
+			<td>17</td>
+			<td>0</td>
+		</tr>
+		<tr>
+			<td>18.</td>
+			<td>DisConnected</td>
+			<td>4</td>
+			<!-- round 1 -->
+			<td>18</td>
+			<td>0</td>
+			<!-- round 2 -->
+			<td>8</td>
+			<td>1</td>
+			<!-- round 3 -->
+			<td>16</td>
+			<td>0</td>
+			<!-- round 4 -->
+			<td>12</td>
+			<td>0</td>
+			<!-- round 5 -->
+			<td>18</td>
+			<td>0</td>
+		</tr>
+		<tr>
+			<td>19.</td>
+			<td>TEAM FP</td>
+			<td>4</td>
+			<!-- round 1 -->
+			<td>17</td>
+			<td>0</td>
+			<!-- round 2 -->
+			<td>20</td>
+			<td>0</td>
+			<!-- round 3 -->
+			<td>17</td>
+			<td>0</td>
+			<!-- round 4 -->
+			<td>17</td>
+			<td>0</td>
+			<!-- round 5 -->
+			<td>5</td>
+			<td>0</td>
+		</tr>
+		<tr>
+			<td>20.</td>
+			<td>INEXPLICATE TEAM</td>
+			<td>2</td>
+			<!-- round 1 -->
+			<td>15</td>
+			<td>0</td>
+			<!-- round 2 -->
+			<td>18</td>
+			<td>0</td>
+			<!-- round 3 -->
+			<td>19</td>
+			<td>0</td>
+			<!-- round 4 -->
+			<td>15</td>
+			<td>0</td>
+			<!-- round 5 -->
+			<td>20</td>
+			<td>0</td>
+		</tr>
+	</tbody>
+</table>
