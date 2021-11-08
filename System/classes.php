62154425a779f42cfa305eabcf49e5c71df254a6
zMango <83130766+CaioSimioni@users.noreply.github.com>
2021-11-08 15:06:00 -0300

    Update classes.php

--- System/classes.php
+++ System/classes.php
@@ -587,12 +587,174 @@ class Chat{
         return $this->chats;
     }
 
+    public function exibirMensagensChat(){
+        global $pdo;
+
+        $sql = $pdo->prepare("SELECT * FROM `chat` WHERE `chat` = 'chat-geral' ORDER BY `data` DESC LIMIT 50");
+        $sql->execute();
+        
+        if($sql->rowCount() > 0){
+
+            $dados = $sql->fetchAll();
+
+            foreach($dados as $values){
+
+                /*  Estrutura das mensagens
+                    <div class="msg-in OU msg-out">  'out' do usuario local
+                        <div class="info-user">
+                            <img src="../Materials/UsersAvatar/icon.png">
+                            <h1> Pedrinho_gamer </h1>
+                        </div>
+                        <div class="content-msg">
+                            <p> Oi, alguém no chat? </p>
+                        </div>
+                    </div> 1
+                */
+                if($values['id_usuario'] == $_SESSION['codigo_usuario']){
+                    
+                    ?>
+                    <div class="vai-pa-direita">
+                        <div class="msg-out">
+                            <div class="info-user">
+                                <img class="avatar-user" src=<?php echo "../Materials/UsersAvatar/".$values['foto_usuario']; ?> alt="img-user">
+                                <h1 class="name-user"><?php echo $values['nome_usuario'] ?></h1>
+                            </div>
+                            <div class="content-msg">
+                                <p class="texto"><?php echo $values['texto'] ?></p>
+                            </div>
+                        </div>
+                    </div>
+                    <?php
+                }else{
+                    ?>
+                    <div class="vai-pa-esquerda">
+                        <div class="msg-in">
+                            <div class="info-user">
+                                <img class="avatar-user" src=<?php echo "../Materials/UsersAvatar/".$values['foto_usuario']; ?> alt="img-user">
+                                <h1 class="name-user"><?php echo $values['nome_usuario'] ?></h1>
+                            </div>
+                            <div class="content-msg">
+                                <p class="texto"><?php echo $values['texto'] ?></p>
+                            </div>
+                        </div>
+                    </div>
+                    <?php
+                }
+
+            }
+            return true;
+
+        }else{
+            //  Exibir que não há mensagens no chat
+            //  Falar para a pessoa ser a primeira
+            ?>
+                <div class="msg-erro">
+                    <div class="title-msg">
+                    <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
+                    width="28px" height="28px" viewBox="0 0 1218.000000 1280.000000"
+                    preserveAspectRatio="xMidYMid meet">
+                    <metadata>
+                    Created by potrace 1.15, written by Peter Selinger 2001-2017
+                    </metadata>
+                    <g transform="translate(0.000000,1280.000000) scale(0.100000,-0.100000)"
+                    fill="#FFFFFF" stroke="none">
+                    <path d="M9020 12775 c-955 -264 -1655 -1028 -1820 -1986 -55 -312 -39 -713
+                    40 -1017 l19 -77 -31 -45 c-155 -221 -602 -750 -998 -1180 -462 -503 -1444
+                    -1500 -1945 -1976 -674 -639 -1234 -1137 -1647 -1462 l-117 -92 -91 0 c-172 0
+                    -423 -39 -625 -97 -738 -212 -1344 -758 -1624 -1461 -76 -191 -126 -379 -162
+                    -617 -17 -105 -17 -484 -1 -605 49 -363 169 -710 352 -1010 60 -98 85 -128
+                    119 -141 34 -13 83 5 103 39 9 15 144 377 298 803 155 425 300 812 322 859 52
+                    109 166 227 273 282 138 72 306 97 447 67 70 -15 1213 -431 1303 -474 178 -86
+                    319 -268 355 -456 15 -80 11 -226 -9 -299 -9 -36 -153 -438 -320 -895 -317
+                    -867 -315 -862 -275 -907 37 -39 79 -35 293 33 824 263 1455 935 1665 1774 68
+                    271 91 556 66 817 -23 240 -84 502 -165 703 l-44 109 87 101 c915 1062 2329
+                    2494 3307 3351 450 393 811 678 1108 873 l97 64 258 2 c280 1 379 12 597 65
+                    460 111 852 329 1186 660 399 396 628 858 720 1455 17 105 17 484 1 605 -39
+                    290 -118 554 -241 805 -64 130 -150 276 -184 314 -53 57 -123 47 -156 -22 -11
+                    -23 -146 -388 -301 -812 -154 -423 -296 -804 -317 -845 -48 -99 -167 -220
+                    -268 -272 -138 -72 -306 -97 -447 -67 -70 15 -1213 431 -1303 474 -140 68
+                    -255 189 -315 332 -38 91 -50 153 -50 259 0 118 10 153 132 489 60 165 200
+                    547 310 849 219 604 218 598 155 636 -40 25 -51 24 -157 -5z"/>
+                    </g>
+                    </svg>
+                        <h1 class="title-msg-erro">Ocorreu um Erro</h1>
+                    </div>
+                    <div class="desc-msg">
+                        <p class="desc-msg-erro">Chat está indisponivel, tente mais tarde!</p>
+                    </div>
+                </div>
+            <?php
+            return false;
+        }
+    }
+
+    public function exibirMensagensAdm(){
+        global $pdo;
+
+        $sql = $pdo->prepare("SELECT * FROM `chat` ORDER BY `chat`.`data` DESC");
+        $sql->execute();
+
+        if($sql->rowCount() > 0){
+
+            $dados = $sql->fetchAll();
+
+            foreach($dados as $key => $values){
+                ?>
+                    <tr>
+                        <td><?php echo $values['id_mensagem']?></td>
+                        <td><?php echo $values['chat']?></td>
+                        <td><?php echo $values['texto']?></td>
+                        <td><?php echo date("d/m/Y H:i", strtotime($values['data']))?></td>
+                        <td class="func"><a class='excluir' href='excluir.php?idmsg=<?php echo $values['id_mensagem']?>'>Excluir</a></td>
+                    </tr>
+                <?php
+            }
+
+            return true;
+
+        }else{
+            return false;
+        }
+    }
+
+    public function deletaMensagem($id_msg){
+        global $pdo;
+
+        $sql = $pdo->prepare("SELECT `nome_usuario` FROM `chat` WHERE `chat`.`id_mensagem` = $id_msg");
+        $sql->execute();
+
+        if($sql->rowCount() > 0){  // Existe a mensagem
+
+            $dados = $sql->fetchAll();
+
+            $nome_usuario = $dados[0]['nome_usuario'];
+            
+            $sql2 = $pdo->prepare("DELETE FROM `chat` WHERE `chat`.`id_mensagem` = :id");
+            $sql2->bindValue(':id', $id_msg);
+            $sql2->execute();
+
+            if($sql2->rowCount() > 0){
+
+                return [
+                    "id" => $id_msg,
+                    "user" => $nome_usuario
+                ];
+
+            }else{
+                return false;
+            }
+
+        }else{
+            return false;
+        }
+    }
+
     public function inserirMensagem($chat, $id_usuario, $nome_usuario, $foto_usuario, $texto){
         global $pdo;
 
         $sql = $pdo->prepare("INSERT INTO `chat`(`chat`, `id_usuario`, `nome_usuario`, `foto_usuario`, `texto`, `data`) VALUES('$chat', $id_usuario, '$nome_usuario', '$foto_usuario', :txt, :dt)");
         $sql->bindValue(':txt', $texto);
-        $sql->bindValue('dt', date('Y-m-d H:i:s'));
+        $sql->bindValue(':dt', date('Y-m-d H:i:s'));
         $sql->execute();
 
         if($sql->rowCount() > 0){
