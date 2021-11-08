eafa1390aedd79e9312c24bf3d97b786a125af5f
zMango <83130766+CaioSimioni@users.noreply.github.com>
2021-11-08 15:05:45 -0300

    Update chat.php

--- Pages/chat.php
+++ Pages/chat.php
@@ -3,10 +3,13 @@
 require "../System/classes.php";
 $user = new Usuario;
 $banco = new BancoBD;
+$chat = new Chat;
 
-
-
-$banco->conectar();
+if(!$banco->conectar()){
+    echo "<script> alert('Não foi possível acessar o site.')</script>";    
+    echo "<script>window.location.href='../index.php'</script>";
+    exit;
+}
 
 if(!isset($_SESSION['logged']) or !isset($_SESSION['codigo_usuario'])){
     echo "<script> alert('Falha na autenticação de usuário!')</script>";    
@@ -14,63 +17,10 @@ if(!isset($_SESSION['logged']) or !isset($_SESSION['codigo_usuario'])){
     exit;
 }
 
-global $pdo;
+// $chat->exibirMensagensChat()
 
-$sql = $pdo->query("SELECT * FROM `chat` WHERE `chat` = 'chat-geral' ORDER BY `data` DESC LIMIT 50");
-
-if($sql->rowCount() > 0){
-
-    $dados = $sql->fetchAll();
-
-    foreach($dados as $values){
-
-        /*  Estrutura das mensagens
-            <div class="msg-in OU msg-out">  'out' do usuario local
-                <div class="info-user">
-                    <img src="../Materials/UsersAvatar/icon.png">
-                    <h1> Pedrinho_gamer </h1>
-                </div>
-                <div class="content-msg">
-                    <p> Oi, alguém no chat? </p>
-                </div>
-            </div> 1
-        */
-        if($values['id_usuario'] == $_SESSION['codigo_usuario']){
-            
-            ?>
-            <div class="vai-pa-direita">
-                <div class="msg-out">
-                    <div class="info-user">
-                        <img class="avatar-user" src=<?php echo "../Materials/UsersAvatar/".$values['foto_usuario']; ?> alt="img-user">
-                        <h1 class="name-user"><?php echo $values['nome_usuario'] ?></h1>
-                    </div>
-                    <div class="content-msg">
-                        <p class="texto"><?php echo $values['texto'] ?></p>
-                    </div>
-                </div>
-            </div>
-            <?php
-        }else{
-            ?>
-            <div class="vai-pa-esquerda">
-                <div class="msg-in">
-                    <div class="info-user">
-                        <img class="avatar-user" src=<?php echo "../Materials/UsersAvatar/".$values['foto_usuario']; ?> alt="img-user">
-                        <h1 class="name-user"><?php echo $values['nome_usuario'] ?></h1>
-                    </div>
-                    <div class="content-msg">
-                        <p class="texto"><?php echo $values['texto'] ?></p>
-                    </div>
-                </div>
-            </div>
-            <?php
-        }
-
-    }
-
-}else{
-    //  Exibir que não há mensagens no chat
-    //  Falar para a pessoa ser a primeira
+if($chat->exibirMensagensChat()){
+    // O chat está funcionando não precisa de nada.
 }
 
 ?>
