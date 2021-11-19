8861b35f0c218b8d50d58fe2e349c48ab925a907
zMango <83130766+CaioSimioni@users.noreply.github.com>
2021-11-18 22:31:10 -0300

    Update campeonatoSelecionar.php

--- Esports/campeonatoSelecionar.php
+++ Esports/campeonatoSelecionar.php
@@ -20,7 +20,11 @@ if($_SESSION['adm'] == false) {
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-    <?php include "../Templates/head.php"?>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Polo</title>
+    <link rel="shortcut icon" href="../Materials/polo_icon.png" type="image/x-icon">
     <link rel="stylesheet" href="../Css/style_adm.css">
     <link rel="stylesheet" href="../Css/style_campeonatoSelecionar.css">
 
